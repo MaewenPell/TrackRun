aeced3a1e5ba1f66a65584a21074f702519c5b56
Maewen Pelletier <mpelletier@tihive.onmicrosoft.com>
2022-12-07 16:00:49 +0100

    Add nav

--- track-run/src/app/app.module.ts
+++ track-run/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from 'src/components/navbar/navbar.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
